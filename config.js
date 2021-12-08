e6cbcb3bd9baacaa8f658d225def8db3008b4a08
robert-dani <robert.dani@onrobot.com>
2021-12-08 13:46:04 +0100

    init

---
+++ config.js
@@ -0,0 +1,21 @@
+module.export =
+{
+    products: [
+        {
+            name: "TENGER GYÜMÖLCSEI 1000G",
+            shortName: "tenger-gyumolcsei-1000g",
+            categories: [""],
+            page: "https://shopsuleiman.hu/tenger-gyumolcsei-1000g-4358",
+            data: [
+                {
+                    name: "price",
+                    selector: "",
+                },
+                {
+                    name: "weight",
+                    selector: "",
+                },
+            ],
+        }
+    ],
+}
